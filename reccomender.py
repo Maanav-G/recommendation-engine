fee3e54755bce8f55d065b0ac525533e8db54062
Maanav Garg <maanavgarg@icloud.com>
2019-06-25 01:32:58 -0400

    basic working model

--- reccomender.py
+++ reccomender.py
@@ -4,11 +4,6 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity 
 
-def get_title_from_index(index):
-    return dataFrame[dataFrame.index == index]["title"].values[0]
-
-def get_index_from_title(title):
-    return dataFrame[dataFrame.title == title]["index"].values[0]
 
 #1 CSV
 dataFrame = pd.read_csv("./dataset/movie_dataset.csv")
@@ -42,7 +37,22 @@ movie_user_likes = "Avatar"
 
 
 #6 Get index of the movie from the title 
+def get_index_from_title(title):
+    return dataFrame[dataFrame.title == title]["index"].values[0]
+
+movie_index = get_index_from_title(movie_user_likes)
+similar_movies = list(enumerate(cosine_sim[movie_index]))
 
 #7 Get a list of similar movies, in order of similarity score 
+sorted_similar_movies = sorted(similar_movies,key= lambda x:x[1], reverse=True)
 
-#8 Print titles of first 50 movies 
+#8 Print titles of first 10 movies 
+def get_title_from_index(index):
+    return dataFrame[dataFrame.index == index]["title"].values[0]
+
+i=0 
+for movie in sorted_similar_movies:
+    print get_title_from_index(movie[0])
+    i=i+1
+    if i>10:
+        break
